312560d197acfe0321f9895478822f90b4ad5852
agape1225 <scg9268@gmail.com>
2023-12-03 23:54:36 +0900

    ✨ final update for assignment
    마지막 업데이트
    - 주석 정리
    - socket disconnect log 생성

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-// const hbs = require("hbs")
-const LogInCollection = require("../db/mongo");
+const LogInCollection = require("../db/login");
 const webSocket = require('../controller/chatController'); 
 const port = process.env.PORT || 3007;
-app.use(express.json());
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const tempelatePath = path.join(__dirname, '../tempelates');
 const publicPath = path.join(__dirname, '../public');
-console.log(publicPath);
 
 app.set('view engine', 'hbs');
 app.set('views', tempelatePath);
@@ -43,7 +41,6 @@ app.post('/signup', async (req, res) => {
     }
     return res.redirect("/");
 })
-
 
 app.post('/login', async (req, res) => {
     try {
